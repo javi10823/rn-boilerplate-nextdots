dad590ba49587b2cb4674c8a9aeac3b6da860c0e
Edgar Andres Bonilla Grajales <edgar.a.bonilla.g@gmail.com>
2020-01-05 01:34:06 -0500

    Change Apollo Link library to 'apollo-upload-client' to support file or image uploads when needed.

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,15 +1,11 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import { createUploadLink } from 'apollo-upload-client';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import { USER_TOKEN } from '../utils/constant';
 import Config from '../config';
-
-const httpLink = new HttpLink({
-  uri: Config.API_URL,
-});
 
 const authLink = setContext(async (req, { headers }) => {
   const token = await AsyncStorage.getItem(USER_TOKEN);
@@ -22,10 +18,17 @@ const authLink = setContext(async (req, { headers }) => {
   };
 });
 
+const uploadCapableLink = createUploadLink({ uri: Config.API_URL });
+
+export async function renewLink() {
+  client.link = authLink.concat(uploadCapableLink);
+}
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(uploadCapableLink),
   cache: new InMemoryCache(),
   connectToDevTools: __DEV__,
+  resolvers: {},
 });
 
 export default client;
